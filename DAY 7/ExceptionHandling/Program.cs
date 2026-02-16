b8785e9df185c551a8e23bb8c927a4612bf80d1a
Sakshi Lahoti <sakshilahoti2603@gmail.com>
2026-02-16 22:32:05 +0530

    Exception Handling - Loggerdemo

---
+++ DAY 7/ExceptionHandling/Program.cs
@@ -0,0 +1,102 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        try
+        {
+            First();
+        }
+        catch (DivideByZeroException ex)
+        {
+            Console.WriteLine($"An error occurred: {ex.Message}");
+            Console.WriteLine($"Stack Trace: {ex.StackTrace}");
+        }
+        // Generic Exceptions towards the end
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred: {ex.Message}");
+            Console.WriteLine($"Stack Trace: {ex.StackTrace}");
+            Console.WriteLine($"Inner Exception: {ex.InnerException}");
+        }
+        finally
+        {
+            // house keeping
+            Console.WriteLine("finally.");
+        }
+
+        Console.WriteLine("Program continues after handling the exception.");
+    }
+
+    static void First()
+    {
+        Second();
+    }
+
+    static void Second()
+    {
+        try
+        {
+            Third();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Exception From Third: {ex.Message}");
+
+            // Good rethrow
+            // throw;
+
+            // Bad rethrow
+            // throw ex;
+
+            // Wrapping exception (current active code)
+            throw new Exception("My Exception here", ex);
+        }
+    }
+
+    static void Third()
+    {
+        var numerator = 10;
+        var denominator = 0;
+
+        var result = numerator / denominator;
+    }
+
+    static void AcceptPayment(decimal amount, decimal balance)
+    {
+        if (amount > balance)
+        {
+            throw new NotEnoughBalanceException("Not enough balance to complete the payment.");
+        }
+
+        Console.WriteLine("Payment accepted.");
+    }
+}
+
+class BankException : ApplicationException
+{
+    public BankException(string message) : base(message)
+    {
+    }
+
+    public BankException(string message, Exception innerException)
+        : base(message, innerException)
+    {
+    }
+}
+
+class NotEnoughBalanceException : BankException
+{
+    public NotEnoughBalanceException(string message)
+        : base(message)
+    {
+    }
+
+    public NotEnoughBalanceException(string message, Exception innerException)
+        : base(message, innerException)
+    {
+    }
+}
+
---
+++ DAY 7/loggerdemo/Program.cs
@@ -0,0 +1,64 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+
+var services = new ServiceCollection();
+
+// Configure Logging Services
+services.AddLogging(config =>
+{
+    
+    config.AddSerilog(new LoggerConfiguration()
+        .WriteTo.Console()            
+        .MinimumLevel.Debug()         
+        .CreateLogger(), dispose: true);
+
+    // Add default console logging
+    config.AddConsole();
+
+  
+    config.SetMinimumLevel(LogLevel.Debug);
+});
+
+
+services.AddScoped<UserService>();
+
+// Build the Service Provider
+var serviceProvider = services.BuildServiceProvider();
+
+
+var userService = serviceProvider.GetService<UserService>();
+
+// Call method to create user
+userService?.CreateUser("John");
+
+
+// UserService class
+public class UserService
+{
+    // ILogger injected using Dependency Injection
+    private readonly ILogger<UserService> _logger;
+    public UserService(ILogger<UserService> logger)
+    {
+        _logger = logger;
+    }
+
+    public void CreateUser(string name)
+    {
+        // Information log before starting user creation
+        _logger.LogInformation("Creating user: {Name}", name);
+
+        try
+        {
+            
+            _logger.LogInformation("User {Name} created successfully", name);
+        }
+        catch (Exception ex)
+        {
+            // Logging error with exception details
+            _logger.LogError(ex, "Failed to create user: {Name}", name);
+        }
+    }
+}
+
